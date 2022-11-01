5a8836589d858cacf8651760ca5972da56508bef
Andrew Cornejo <atcornejo3@gmail.com>
2022-11-01 15:18:12 -0700

    fleshed out app.js, zodiacs controller with zodiacs-data, and adjusted app.test.js. Receiving a failed test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,13 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const { zodiacs } = require('../lib/zodiacs-data');
 
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
+describe('zodiac routes', () => {
+  it('/zodiacs should return list of zodiacs', async () => {
+    const res = await request(app).get('/zodiacs');
+    const expected = zodiacs.map((zodiac) => {
+      return { id: zodiac.id, name: zodiac.name };
+    });
+    expect(res.body).toEqual(expected);
   });
 });
-
-
-//this is just a tribute
